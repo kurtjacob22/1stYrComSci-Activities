07e925c5f56db64caabd8ede3f869e640bd35901
kurtjacob22 <kurtjacob.urquico@gmail.com>
2021-01-28 13:09:59 +0800

    missing number

---
+++ CSES Problem Set/missingnumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long int n, sum = 0;
+    cin >> n;
+
+    for(int i = 1; i < n; i++){
+        int num;
+        cin >> num;
+        sum += num;
+    }
+    cout << n * (n + 1) / 2 - sum;
+}
